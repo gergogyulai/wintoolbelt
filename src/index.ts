d1263591b058f626ed0a44dfb8f22b974d6aea73
gergo <113334790+gergogyulai@users.noreply.github.com>
2025-03-14 12:17:09 +0100

    add useragent validation, and docs request handler

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,21 @@ import { Hono } from 'hono'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('*', async (c) => {
-  const response = await fetch('https://raw.githubusercontent.com/gergogyulai/winwipe/main/public/wipe.ps1', {
-    headers: { 'Content-Type': 'text/plain' }
-  })
-  return response
+app.get('/', async (c) => {
+  const userAgent = c.req.header('User-Agent')
+
+  if (userAgent?.includes("PowerShell")) {
+    const response = await fetch('https://raw.githubusercontent.com/gergogyulai/winwipe/main/public/wipe.ps1', {
+      headers: { 'Content-Type': 'text/plain' }
+    })
+    return response
+  } else {
+    return c.text('To run this script, use the following command in PowerShell:\n\nirm https://winwipe.gergo.cc | iex \n\n\ Check the docs at: "https://winwipe/gergo.cc/docs', 200)
+  }
+})
+
+app.get("/docs", async (c)=>{
+  c.redirect('https://github.com/gergogyulai/winwipe', 301)
 })
 
 
